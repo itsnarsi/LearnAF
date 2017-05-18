2573969f49158fa46b6e154097f2a9b22d2c2eb1
Narsi Reddy <narsi2992@gmail.com>
2017-05-18 01:44:02 -0500

    Works Well!
    Had problem with training data. Had wrong data to test the framework.

--- LearnAF/nn/metrics.py
+++ LearnAF/nn/metrics.py
@@ -4,6 +4,10 @@ from collections import namedtuple
 from operator import mul
 from functools import reduce
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 def accuracy(Y,YP):
-    return af.mean(Y.eval() * af.arith.round(YP.eval()))
+    P = np.argmax(np.asarray(YP.eval()), axis=1)
+    T = np.argmax(np.asarray(Y.eval()), axis=1)
+
+    return accuracy_score(T, P)
